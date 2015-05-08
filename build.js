9b798404ecef575a302884a008c1aaf6b43988bb
Philipp Smirnov <smirnfil@gmail.com>
2015-05-08 17:15:29 +1000

    Serve functionality added - could be served with npm run start

--- build.js
+++ build.js
@@ -1,10 +1,19 @@
 "use strict";
 
+
+var stdio = require('stdio');
+
+var ops = stdio.getopt({'serve': {}});
+
+
 var metalsmith = require('metalsmith');
+var serve = ops.serve ? require('metalsmith-serve') : function(){};
+
 
 metalsmith(__dirname)
 	.source('src')
 	.destination('build')
+	.use(serve())
 	.build(function (err) {
 		if (err) {
 			throw err;
