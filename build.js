e89f3c36569aa7f1fc51ce1a05a46773923db880
Philipp Smirnov <smirnfil@gmail.com>
2015-05-09 13:49:44 +1000

    metalsmith-markdown set up

--- build.js
+++ build.js
@@ -2,20 +2,20 @@
 
 
 var stdio = require('stdio');
-
-var ops = stdio.getopt({'serve': {}});
-
+var ops = stdio.getopt({ 'serve': {} });
 
 var metalsmith = require('metalsmith');
-var serve = ops.serve ? require('metalsmith-serve') : function(){};
+var serve = ops.serve ? require('metalsmith-serve') : function () { };
+var markdown = require('metalsmith-markdown');
 
 
 metalsmith(__dirname)
 	.source('src')
 	.destination('build')
-	.use(serve())
+	.use(markdown())
+	.use(serve())	
 	.build(function (err) {
-		if (err) {
-			throw err;
-		}
-	});
+	if (err) {
+		throw err;
+	}
+});
