fb695243c3d07e18d66ab28f1c43d91114018a64
Philipp Smirnov <smirnfil@gmail.com>
2015-05-11 13:34:29 +1000

    autoregistartion of partial templates

--- build.js
+++ build.js
@@ -11,12 +11,19 @@ var templates  = require('metalsmith-templates');
 var Handlebars = require('handlebars');
 var fs         = require('fs');
 
-Handlebars.registerPartial('article', fs.readFileSync(__dirname + 
+var partials = fs.readdirSync(__dirname + '/templates/partials');
+
+for (var i = 0; i < partials.length; i++) {
+		Handlebars.registerPartial(partials[i].split('.')[0], fs.readFileSync(__dirname + '/templates/partials/' + partials[i]).toString());
+}
+	
+
+/*Handlebars.registerPartial('article', fs.readFileSync(__dirname + 
 '/templates/partials/article.html').toString());
 
 Handlebars.registerPartial('posts', fs.readFileSync(__dirname + 
 '/templates/partials/posts.html').toString());
-
+*/
 metalsmith(__dirname)
 	.source('src')
 	.use(markdown())
